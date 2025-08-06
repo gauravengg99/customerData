a304fa85e8356076f394cd057201815dc2d81934
gauravengg99 <sales2.gauravengg@gmail.com>
2025-08-06 05:01:17 +0000

    change urlencoded

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config({path:'.env'})
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.urlencoded({extends:true}))
 app.use(express.urlencoded({extended:true}))
 const port = process.env.PORT || 4000
 
